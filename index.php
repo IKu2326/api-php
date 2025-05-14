499803bd6bd45d268477bf419611ed8f57b2d74f
Karen-alvarez04 <Karen-alvarez04>
2025-05-14 11:12:25 -0500

    Api funcional

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header('Access-Control-Allow-Origin: http://localhost:5173');
+    header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+    header('Access-Control-Allow-Headers: Content-Type, Authorization');
+    exit(0);
+}
+require_once './controllers/controladorUsuario.php';
+require_once './controllers/controladorMostrarProducto.php';
+require_once './controllers/controladorDetallesProducto.php';
+
+$ruta = trim($_GET['ruta'] ?? '');
+
+switch ($ruta) {
+    case 'registrar':
+        ControladorUsuario::registrar();
+        break;
+    case 'login':
+        ControladorUsuario::login();
+        break;
+    case 'obtenerProductosDesc':
+        ControladorMostrarProducto::productosMasVendidos();
+        break;
+    case 'obtenerProductosTendencias':
+        ControladorMostrarProducto::productosTendencias();
+        break;
+    case 'obtenerProductosExclusivos':
+        ControladorMostrarProducto::productosExclusivos();
+        break;
+    case 'obtenerDetallesProducto':
+        ControladorMostrarDetallesProducto::mostrarDetallesProducto();
+        break;
+    default:
+        echo json_encode(["mensaje" => "Ruta no encontrada.", "ruta_solicitada" => $ruta]);
+        break;
+}
